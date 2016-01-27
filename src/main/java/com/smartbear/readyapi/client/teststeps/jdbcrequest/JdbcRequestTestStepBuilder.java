978d140b5933f6379a15bdda38e405af9e3a0092
spindelmanne <manne.fagerlind@smartbear.com>
2016-01-27 16:50:24 +0100

    Fix typo in Javadoc

--- src/main/java/com/smartbear/readyapi/client/teststeps/jdbcrequest/JdbcRequestTestStepBuilder.java
+++ src/main/java/com/smartbear/readyapi/client/teststeps/jdbcrequest/JdbcRequestTestStepBuilder.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
- * Builder for JdbcRequestTestStepobjects.
+ * Builder for JdbcRequestTestStep objects.
  */
 public class JdbcRequestTestStepBuilder implements TestStepBuilder<JdbcRequestTestStep> {
 
