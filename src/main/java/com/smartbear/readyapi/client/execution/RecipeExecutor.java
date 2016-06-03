584295cf2c7530dcf15b64cba5b44be95c306cbb
Prakash Jat <prakash.jat@smartbear.com>
2016-06-03 10:46:53 +0200

    TSERV-153 : Use simple slf4j logger

--- src/main/java/com/smartbear/readyapi/client/execution/RecipeExecutor.java
+++ src/main/java/com/smartbear/readyapi/client/execution/RecipeExecutor.java
@@ -10,11 +10,9 @@ import com.smartbear.readyapi.client.model.TestCase;
 import com.smartbear.readyapi.client.model.TestStep;
 import com.smartbear.readyapi.client.model.UnresolvedFile;
 import io.swagger.client.auth.HttpBasicAuth;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
@@ -26,7 +24,14 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class RecipeExecutor {
 
-    private static Logger logger = LoggerFactory.getLogger(RecipeExecutor.class);
+    private static Logger logger;
+
+    static {
+        if (System.getProperty("org.slf4j.simpleLogger.defaultLogLevel") == null) { //Don't set if user has defined the log level
+            System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "trace");
+        }
+        logger = LoggerFactory.getLogger(RecipeExecutor.class);
+    }
 
     private static final int NUMBER_OF_RETRIES_IN_CASE_OF_ERRORS = 3;
     private final TestServerApi apiStub;
@@ -110,8 +115,7 @@ public class RecipeExecutor {
             return new Execution(apiStub, authentication, projectResultReport);
         } catch (ApiException e) {
             invokeListeners(e);
-            logger.debug("An error occurred when sending test recipe to server");
-            logger.debug(e.toString());
+            logger.debug("An error occurred when sending test recipe to server. Details: " + e.toString());
         } catch (Exception e) {
             invokeListeners(e);
             logger.debug("An error occurred when sending test recipe to server", e);
