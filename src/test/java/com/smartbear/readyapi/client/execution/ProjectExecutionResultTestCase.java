1a8decd8f09381b1c8a301d4e590b8294128615c
Prakash Jat <prakash.jat@smartbear.com>
2016-07-27 17:55:39 +0200

    TSERV-167 : get assertion status for the given assertion

--- src/test/java/com/smartbear/readyapi/client/execution/ProjectExecutionResultTestCase.java
+++ src/test/java/com/smartbear/readyapi/client/execution/ProjectExecutionResultTestCase.java
@@ -2,6 +2,7 @@ package com.smartbear.readyapi.client.execution;
 
 import com.smartbear.readyapi.client.model.HarLogRoot;
 import com.smartbear.readyapi.client.model.ProjectResultReport;
+import com.smartbear.readyapi.client.model.TestStepResultReport;
 import com.smartbear.readyapi.client.result.RecipeExecutionResult;
 import io.swagger.client.auth.HttpBasicAuth;
 import io.swagger.util.Json;
@@ -11,8 +12,12 @@ import org.mockito.Matchers;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import static com.smartbear.readyapi.client.model.TestStepResultReport.AssertionStatusEnum.FAILED;
+import static com.smartbear.readyapi.client.model.TestStepResultReport.AssertionStatusEnum.OK;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -23,17 +28,17 @@ public class ProjectExecutionResultTestCase {
     public void testExecutionResult() throws IOException {
 
         ProjectResultReport resultReport =
-            Json.mapper().readValue(new FileInputStream("src/test/resources/project-result-report.json"),
-                ProjectResultReport.class);
+                Json.mapper().readValue(new FileInputStream("src/test/resources/project-result-report.json"),
+                        ProjectResultReport.class);
 
         HarLogRoot harLogRoot =
-            Json.mapper().readValue(new FileInputStream("src/test/resources/single-entry-har-log.json"),
-                HarLogRoot.class);
+                Json.mapper().readValue(new FileInputStream("src/test/resources/single-entry-har-log.json"),
+                        HarLogRoot.class);
 
-        TestServerApi apiMock = mock( TestServerApi.class );
-        when( apiMock.getTransactionLog(Matchers.anyString(), Matchers.anyString(), (HttpBasicAuth) Matchers.any())).thenReturn( harLogRoot );
+        TestServerApi apiMock = mock(TestServerApi.class);
+        when(apiMock.getTransactionLog(Matchers.anyString(), Matchers.anyString(), (HttpBasicAuth) Matchers.any())).thenReturn(harLogRoot);
 
-        Execution execution = new Execution( apiMock, new HttpBasicAuth(), resultReport );
+        Execution execution = new Execution(apiMock, new HttpBasicAuth(), resultReport);
         RecipeExecutionResult result = execution.getExecutionResult();
 
         assertEquals(ProjectResultReport.StatusEnum.FINISHED, result.getStatus());
@@ -45,12 +50,14 @@ public class ProjectExecutionResultTestCase {
         assertEquals(2, result.getTestStepResults("GET request 1").size());
         assertEquals(1, result.getErrorMessages().size());
         assertEquals(1, result.getTestStepResult(1).getMessages().size());
+        assertThat(FAILED, is(result.getTestStepResult(1).getStatusForAssertion("Valid HTTP Status Codes")));
+        assertThat(OK, is(result.getTestStepResult(1).getStatusForAssertion("Invalid HTTP Status Codes")));
 
         assertNotNull(result.getFirstTestStepResult("get request 1"));
         assertNotNull(result.getLastTestStepResult("get request 1"));
         assertEquals(1, result.getFailedTestStepsResults("GET request 1").size());
 
-        assertTrue( result.getTestStepResult( 0 ).hasTransactionData());
-        assertEquals( "Test response", result.getTestStepResult( 0 ).getResponseContent());
+        assertTrue(result.getTestStepResult(0).hasTransactionData());
+        assertEquals("Test response", result.getTestStepResult(0).getResponseContent());
     }
 }
