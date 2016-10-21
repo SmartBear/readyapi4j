2bfbc1c342d1e86e4de90f31719b788d556df9ac
Ole Lensmar <ole.lensmar@smartbear.com>
2016-10-21 08:25:23 +0200

    improved utility methods for expansion

--- src/main/java/com/smartbear/readyapi/client/support/Expanders.java
+++ src/main/java/com/smartbear/readyapi/client/support/Expanders.java
@@ -7,19 +7,23 @@ package com.smartbear.readyapi.client.support;
 public class Expanders {
     public static final String DEFAULT_DATASOURCE_TESTSTEP_NAME = "DataSource";
 
-    public static String expand(String property, String testStep, String path) {
+    public static String expand(String testStep, String property, String path) {
         return "${" + testStep + "#" + property + "#" + path + "}";
     }
 
     public static String fromResponse(String testStep, String path) {
-        return expand("Response", testStep, path);
+        return expand(testStep, "Response", path);
+    }
+
+    public static String script(String script) {
+        return "${=" + script + "}";
     }
 
     public static String dataSourceProperty(String property) {
         return expand(property, DEFAULT_DATASOURCE_TESTSTEP_NAME);
     }
 
-    public static String expand(String property, String testStep) {
+    public static String expand(String testStep, String property) {
         return "${" + testStep + "#" + property + "}";
     }
 }
